38b31f150823919719436f7c83f7f146cbbb4781
Brian Mackey <bmackey@onetechnologies.net>
2015-11-09 11:57:18 -0600

    code cleanup

---
+++ BddDemo/Controllers/PaymentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BddDemo.Lib;
+
+namespace BddDemo.Controllers
+{
+    public class PaymentController : ApiController
+    {
+        private IPayment payment;
+
+        public PaymentController(IPayment payment)
+        {
+            this.payment = payment;
+        }
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public bool Post([FromBody]string creditCardNumber)
+        {
+            var result = payment.Accept(creditCardNumber);
+            return result;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
