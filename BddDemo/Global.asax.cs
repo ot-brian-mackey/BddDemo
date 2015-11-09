55b82f2720a66d85ad5284e39cfc4d3c7ecd404c
Brian Mackey <bmackey@onetechnologies.net>
2015-11-09 13:12:18 -0600

    add IoC.  Add readme

--- BddDemo/Global.asax.cs
+++ BddDemo/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BddDemo.Lib;
+using Ninject;
 
 namespace BddDemo
 {
@@ -17,6 +19,28 @@ namespace BddDemo
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            
+
+            SetupDependencyInjection();
+            //GlobalConfiguration.Configuration.DependencyResolver = new StructureMapDependencyResolver();
+        }
+
+        private void SetupDependencyInjection()
+        {
+            //create Ninject DI Kernel
+            IKernel kernel = new StandardKernel();
+
+            //tell asp.net mvc to use our Ninject DI Container
+            GlobalConfiguration.Configuration.DependencyResolver = new NinjectResolver(kernel);
+
+            //register services with Ninject DI container
+            RegisterServices(kernel);
+        }
+
+        private void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<IPayment>().To<Payment>();
+            kernel.Bind<IVendor>().To<VendorXyz>();
         }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,10 @@
+#About
+
+Demo covering some general concepts including BDD, IoC (ninject), moq,
+
+##Testing
+
+Using Postman you can use the following configuration
+
+- Endpoint: POST to http://localhost/BddDemo/api/Payment
+- Body: raw JSON with value of "1234"
